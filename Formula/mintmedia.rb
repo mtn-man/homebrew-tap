e2686691d721e8f37469c636e1d610f5634414b9
Eli Sher <elijsher@gmail.com>
2026-07-14 16:00:00 -0600

    mintmedia: update formula to v0.1.2

--- Formula/mintmedia.rb
+++ Formula/mintmedia.rb
@@ -1,30 +1,30 @@
 class Mintmedia < Formula
   desc "Organize downloaded media into clean Movies and Shows libraries"
   homepage "https://github.com/mtn-man/mintmedia"
-  version "0.1.1"
+  version "0.1.2"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_intel do
-      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.1/mintmedia_v0.1.1_darwin_amd64.tar.gz"
-      sha256 "87185f2cca6d3498ac998dceb80247233d81d91446b9874556e0a7e78f1e211c"
+      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.2/mintmedia_v0.1.2_darwin_amd64.tar.gz"
+      sha256 "c446ba59ced225e8b97f8c35a1445b6aa973be231bdd045f66f650c9a79facb2"
     end
 
     on_arm do
-      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.1/mintmedia_v0.1.1_darwin_arm64.tar.gz"
-      sha256 "cee4844a88a065081aa58540380e7cc96e35062ffa7bf1da909d10ba86228535"
+      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.2/mintmedia_v0.1.2_darwin_arm64.tar.gz"
+      sha256 "8c946aef576e85c8df787b2a0eaa7499991269934f70685fcc571b0093f30cc2"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.1/mintmedia_v0.1.1_linux_amd64.tar.gz"
-      sha256 "07283c12c3718e25563c2e194d525675a66fe48e59cfd6866bc7820e726dde62"
+      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.2/mintmedia_v0.1.2_linux_amd64.tar.gz"
+      sha256 "302a3aae1ab657da3e973b55b5ba2a6d423b9c8dcdf4c55a068b52abbd136704"
     end
 
     on_arm do
-      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.1/mintmedia_v0.1.1_linux_arm64.tar.gz"
-      sha256 "28810a5cc0c4b987192ffd4852422f6c16e30080c39d257d33487d915f8276e9"
+      url "https://github.com/mtn-man/mintmedia/releases/download/v0.1.2/mintmedia_v0.1.2_linux_arm64.tar.gz"
+      sha256 "2205652bfd2923f4aaa0305a3f8e1f32339ebe80d30c83e43bb28256348d285c"
     end
   end
 
@@ -46,6 +46,6 @@ class Mintmedia < Formula
   end
 
   test do
-    assert_match "mintmedia v0.1.1", shell_output("#{bin}/mintmedia --version")
+    assert_match "mintmedia v0.1.2", shell_output("#{bin}/mintmedia --version")
   end
 end
