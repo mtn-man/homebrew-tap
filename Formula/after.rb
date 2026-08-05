0b54eb56fcb4ea5d8ed79bb5961c72bd8b42739d
Eli Sher <elijsher@gmail.com>
2026-08-05 16:33:51 -0600

    after: update formula to v1.7.0

--- Formula/after.rb
+++ Formula/after.rb
@@ -2,29 +2,29 @@ class After < Formula
   desc "Fast CLI countdown timer with live terminal feedback and optional alarms"
   homepage "https://github.com/mtn-man/after"
   license "MIT"
-  version "1.6.1"
+  version "1.7.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/Mtn-Man/after/releases/download/v1.6.1/after_v1.6.1_darwin_amd64.tar.gz"
-      sha256 "234e878f93dcdb5aa257796a000484ae142d9cc5cf5d2695e9b8b7421b975df6"
+      url "https://github.com/Mtn-Man/after/releases/download/v1.7.0/after_v1.7.0_darwin_amd64.tar.gz"
+      sha256 "9801164d68dda6a32a63df7f596a6d8140a410ad39a9340f293db0a7895102e6"
     end
 
     on_arm do
-      url "https://github.com/Mtn-Man/after/releases/download/v1.6.1/after_v1.6.1_darwin_arm64.tar.gz"
-      sha256 "946be11f193fd0fdb596bea6527598a4546102e98722ae0476f1543e5c7b1217"
+      url "https://github.com/Mtn-Man/after/releases/download/v1.7.0/after_v1.7.0_darwin_arm64.tar.gz"
+      sha256 "6edb798221af03fa5768e134a7a47d4d02fc5d24d3a8bec8d2e360ebe4d917e7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Mtn-Man/after/releases/download/v1.6.1/after_v1.6.1_linux_amd64.tar.gz"
-      sha256 "236130e7c7de93a99917968b1487c36278f308b8886826e1ed73d2288147fa16"
+      url "https://github.com/Mtn-Man/after/releases/download/v1.7.0/after_v1.7.0_linux_amd64.tar.gz"
+      sha256 "b0a46e80743d679c658a344a4b11cabba1833aec21ca0f9abcf6e61a24a897fb"
     end
 
     on_arm do
-      url "https://github.com/Mtn-Man/after/releases/download/v1.6.1/after_v1.6.1_linux_arm64.tar.gz"
-      sha256 "061298ff9327d2bdfeb8ec34a76d5f32c1dd89fb3806137d44144bc63c22ae97"
+      url "https://github.com/Mtn-Man/after/releases/download/v1.7.0/after_v1.7.0_linux_arm64.tar.gz"
+      sha256 "8138ae25fbf7630c0cde7bea318f6a5cb16e63919087233de90ed6a009b802a7"
     end
   end
 
@@ -46,6 +46,6 @@ class After < Formula
   end
 
   test do
-    assert_match "after v1.6.1", shell_output("#{bin}/after --version")
+    assert_match "after v1.7.0", shell_output("#{bin}/after --version")
   end
 end
